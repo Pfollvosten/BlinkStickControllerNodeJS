9e5c34500bd52e40eb99e8ab01c17f3ae379cb61
Lukas <lukas.boehm00@gmx.de>
2018-05-19 22:29:08 +0200

    css and other types in request implementation

--- server.js
+++ server.js
@@ -6,22 +6,43 @@ var fs = require('fs');
 //device setup
 var device = blinkstick.findFirst();
 device.inverse = true;
-//server setup
-server = http.createServer(function (req, res) {
-    console.log('got request....processing');
 
-    pathname = url.parse(req.url , true).pathname.substring(1);
-    if(pathname.substr(pathname.length-4) == 'html'){
-        fs.readFile("./complex/" + pathname ,function (err, data){
-            res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
-            res.write(data);
-            res.end();
-        });  
-    }
+//server setup
+/*server = http.createServer(function (req, res) {
+    
     q = url.parse(req.url, true).query;
     applyColor(q.mode , q.color);
 
-}).listen(8187 , '192.168.178.28');
+}).listen(8187 , '192.168.178.28');*/
+
+
+var server = http.createServer(function (req ,res){
+    console.log('got request...processing');
+
+    fs.readFile('./complex/' + req.url , function(err ,data){
+        if(!err){
+            var dotoffset = req.url.lastIndexOf('.');
+            var mimetype = dotoffset == -1
+                            ? 'text/plain'
+                            : {
+                                '.html' : 'text/html',
+                                '.ico' : 'image/x-icon',
+                                '.jpg' : 'image/jpeg',
+                                '.png' : 'image/png',
+                                '.gif' : 'image/gif',
+                                '.css' : 'text/css',
+                                '.js' : 'text/javascript'
+                            }[ req.url.substr(dotoffset)];
+            res.setHeader('Content-type' , mimetype);
+            res.end(data);
+            console.log( req.url , mimetype);
+        }else{
+            console.log('file not found: ' + req.url);
+            res.writeHead(404, "Not Found");
+            res.end();
+        }
+    });
+}).listen(8187);
 console.log('Server up and running');
 
 //blinkstick control
