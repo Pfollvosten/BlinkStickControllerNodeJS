99461e7122f7c00047c25471187c9d65319cf42a
Lukas <lukas.boehm00@gmx.de>
2018-05-19 15:42:18 +0200

    html und filesteam

--- Client/ajax.js
+++
@@ -1,3 +0,0 @@
-var xhttp = new XMLHttpRequest();
-xhtpp.open("GET","localhost" /* add parameter you get from document html elements  */,true);
-xhttp.send();
--- Server/server.js
+++ Server/server.js
@@ -1,27 +1,36 @@
+//imports
 var http = require("http");
 var url = require('url');
-
 var blinkstick = require('blinkstick');
+var fs = require('fs');
+//device setup
 var device = blinkstick.findFirst();
 device.inverse = true;
-
+//server setup
 server = http.createServer(function (req, res) {
-    console.log('request starting...');
-
-    //respond
-    res.write('hello client!');
-    res.end();
-
-
-    /*
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    var q = url.parse(req.url, true).query;
-    var txt = q.mode + " " + q.color;
-    var mode = q.mode;
-    var color = q.color;
-    device.setColor(color);
-    res.end(mode + " " + color);
-    */
-})
-server.listen(8187);//, '192.168.178.28');
+    console.log('got request....processing');
+    fs.readFile('server.html',function (err, data){
+        res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+        res.write(data);
+        res.end();
+    });
+    
+    q = url.parse(req.url, true).query;
+    applyColor(q.mode , q.color);
+}).listen(8187);//, '192.168.178.28');
 console.log('Server up and running');
+
+//blinkstick control
+function applyColor(mode , color){
+    switch(mode){
+        case "color":
+        device.setColor(color);
+        break;
+        case "flow":
+        device.morph(color, 1, 1);
+        break;
+        case "blink":
+        device.blink(color , 1,1 );
+        break;
+    }
+}
